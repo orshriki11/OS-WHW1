b91401984b32418de4748a2223c590a211e2c0d0
Or S <or.shriki@campus.technion.ac.il>
2024-06-30 20:43:09 +0300

    prior to fix merge v0.1

--- Commands.cpp
+++ Commands.cpp
@@ -91,6 +91,28 @@ char** _initArgs(const char *cmd_line, int *argsCount)
     return args;
 }
 
+void freeArgs(char **args, int arg_num)
+{
+    if (!args)
+        return;
+
+    for (int i = 0; i < arg_num; i++) {
+        if (args[i])
+            free(args[i]);
+    }
+    free(args);
+}
+
+std::string getCurrentPath()
+{
+    char temp[PATH_MAX];
+    if (getcwd(temp, sizeof(temp)) == nullptr) {
+        perror("getcwd");
+        return "";
+    }
+    return std::string(temp);
+}
+
 bool is_number(const std::string& s)
 {
     std::string::const_iterator it = s.begin();
@@ -114,6 +136,132 @@ string _aliasCmd(const char *cmd){
 
 
 // TODO: Add your implementation for classes in Commands.h
+
+//////////////////////////////-------------Built-in Commands-------------//////////////////////////////
+
+/* Chprompt Command - chprompt command will allow the user to change the prompt displayed by
+                      the smash while waiting for the next command.*/
+
+ChpromptCommand::ChpromptCommand(const char *cmd_line) : BuiltInCommand(cmd_line) {}
+
+void ChpromptCommand::execute()
+{
+    int num_of_args;
+    char** args = initArgs(this->cmd_line, &num_of_args);
+    if (args == nullptr) {
+        perror("smash error : malloc failed");
+        return;
+    }
+    SmallShell &shell = SmallShell::getInstance();
+    if (num_of_args == 1) {
+        shell.smash_prompt = "smash";
+    } else {
+        shell.smash_prompt = args[1];
+    }
+    freeArgs(args, num_of_args);
+}
+
+/* ShowPidCommand - showpid command prints the smash PID.*/
+
+ShowPidCommand::ShowPidCommand(const char *cmd_line) : BuiltInCommand(cmd_line) {}
+
+void ShowPidCommand::execute()
+{
+    cout << "smash pid is " << SmallShell::getInstance().pid << endl;
+}
+
+GetCurrDirCommand::GetCurrDirCommand(const char *cmd_line) : BuiltInCommand(cmd_line) {}
+
+void GetCurrDirCommand::execute()
+{
+    string path = getCurrentPath();
+    if(!path.empty())
+        cout << path << endl;
+}
+
+/* CD Command - The cd (Change Directory) command takes a single argument <path> that specifies either a
+                relative or full path to change the current working directory to.
+                There's a special argument, `-`, that cd can accept. When `-` is the only argument provided
+                to the cd command, it instructs the shell to change the current working directory to the
+                last working directory.*/
+ChangeDirCommand::ChangeDirCommand(const char *cmd_line, char **Penultimate) : BuiltInCommand(cmd_line), Penultimate(Penultimate) {}
+
+void ChangeDirCommand::execute()
+{
+    int num_of_args;
+    char **args = init_args(this->cmd_line, &num_of_args);
+    if (!args) {
+        perror("smash error: malloc failed");
+        return;
+    }
+
+    if (num_of_args > 2) {
+        std::cerr << "smash error: cd: too many arguments" << std::endl;
+    }
+    else if (num_of_args == 1) {
+        std::cerr << "smash error: cd: no arguments" << std::endl;
+    }
+    else {
+        long max_size = pathconf(".", _PC_PATH_MAX);
+        char *buffer = (char *) malloc((size_t) max_size);
+        if (!buffer) {
+            perror("smash error: malloc failed");
+            freeArgs(args, num_of_args);
+            return;
+        }
+
+        if (getcwd(buffer, (size_t) max_size) == nullptr) {
+            perror("smash error: getcwd failed");
+            free(buffer);
+            freeArgs(args, num_of_args);
+            return;
+        }
+
+        std::string next = args[1];
+
+        if (next == "-") {
+            if (!(*Penultimate)) {
+                std::cerr << "smash error: cd: OLDPWD not set" << std::endl;
+                free(buffer);
+            } else {
+                if (chdir(*Penultimate) == -1) {
+                    perror("smash error: chdir failed");
+                    free(buffer);
+                    freeArgs(args, num_of_args);
+                    return;
+                } else {
+                    if (*Penultimate) {
+                        free(*Penultimate);
+                    }
+                    *Penultimate = buffer;
+                }
+            }
+        } else {
+            if (chdir(args[1]) == -1) {
+                perror("smash error: chdir failed");
+                freeArgs(args, num_of_args);
+                return;
+            } else {
+                if (*Penultimate) {
+                    free(*Penultimate);
+                }
+                *Penultimate = buffer;
+            }
+        }
+    }
+    freeArgs(args, num_of_args);
+}
+
+JobsCommand::JobsCommand(const char *cmd_line) : BuiltInCommand(cmd_line) {}
+
+void JobsCommand::execute()
+{
+    SmallShell &shell = SmallShell::getInstance();
+
+    shell.jobsList.removeFinishedJobs();
+
+    shell.jobsList.printJobsList();
+}
 
 JobsList SmallShell::jobsList;
 pid_t SmallShell::pid = getppid();
